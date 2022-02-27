ef46e1c1b45dcb60ca346405dfde7dcec9a4b23d
litencatt <ncl0709@gmail.com>
2022-02-27 11:24:30 +0900

    create and update not supported now

--- src/commands/db.ts
+++ src/commands/db.ts
@@ -3,7 +3,7 @@ import {
   QueryDatabaseParameters,
 } from '@notionhq/client/build/src/api-endpoints'
 import { Command, Flags } from '@oclif/core'
-import { retrieveDb, queryDb } from '../notion'
+import { queryDb, createDb, updateDb, retrieveDb } from '../notion'
 
 export default class Db extends Command {
   static description = 'describe the command here'
@@ -16,6 +16,11 @@ export default class Db extends Command {
     query: Flags.boolean({ char: 'q', dependsOn: ['database_id'] }),
     filter: Flags.string({ char: 'f', dependsOn: ['query'] }),
 
+    //create: Flags.boolean({ char: 'c' }),
+    //page_id: Flags.string({ char: 'p', dependsOn: ['c'] }),
+
+    //update: Flags.boolean({ char: 'u' }),
+
     retrieve: Flags.boolean({ char: 'r', dependsOn: ['database_id'] }),
   }
 
@@ -26,6 +31,14 @@ export default class Db extends Command {
       const res = await queryDb(flags.database_id, flags.filter as string)
       console.log(res)
     }
+    // if (flags.create && flags.page_id) {
+    //   const res = await createDb(flags.page_id)
+    //   console.log(res)
+    // }
+    // if (flags.database_id && flags.update) {
+    //   const res = await updateDb(flags.database_id)
+    //   console.log(res)
+    // }
     if (flags.database_id && flags.retrieve) {
       const res = await retrieveDb(flags.database_id)
       console.log(res)
