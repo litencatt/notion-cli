94df8903df72191321001d3e62cde8b23edee074
litencatt <ncl0709@gmail.com>
2023-03-20 17:16:47 +0900

    Do eslint

--- test/commands/page.test.ts
+++ test/commands/page.test.ts
@@ -1,17 +1,17 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from '@oclif/test'
 
 describe('page', () => {
   test
-  .stdout()
-  .command(['page'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
+    .stdout()
+    .command(['page'])
+    .it('runs hello', (ctx) => {
+      expect(ctx.stdout).to.contain('hello world')
+    })
 
   test
-  .stdout()
-  .command(['page', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
+    .stdout()
+    .command(['page', '--name', 'jeff'])
+    .it('runs hello --name jeff', (ctx) => {
+      expect(ctx.stdout).to.contain('hello jeff')
+    })
 })
