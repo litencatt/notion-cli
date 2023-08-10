004f24046f152ff0b2e2686ad38c428f6c1d585a
litencatt <ncl0709@gmail.com>
2023-08-11 01:16:01 +0900

    Promptable db retrieve

--- src/commands/db/retrieve.ts
+++ src/commands/db/retrieve.ts
@@ -1,5 +1,10 @@
 import {Args, Command, Flags} from '@oclif/core'
+import { isFullDatabase } from '@notionhq/client'
 import * as notion from '../../notion'
+import {
+  onCancel,
+} from '../../helper'
+const  prompts  = require('prompts')
 
 export default class DbRetrieve extends Command {
   static description = 'Retrieve a database'
@@ -9,12 +14,46 @@ export default class DbRetrieve extends Command {
   ]
 
   static args = {
-    databaseId: Args.string({required: true}),
+    databaseId: Args.string(),
   }
 
   public async run(): Promise<void> {
     const { args } = await this.parse(DbRetrieve)
-    const res = await notion.retrieveDb(args.databaseId)
+
+    let databaseId = args.databaseId
+    if (databaseId == undefined) {
+      const dbs = await notion.searchDb()
+      const dbChoices = []
+      for (const db of dbs) {
+        if (db.object != "database") {
+          continue
+        }
+        if (!isFullDatabase(db)) {
+          continue
+        }
+        if (db.title[0] == null) {
+          continue
+        }
+        dbChoices.push({
+          title: db.title[0].plain_text,
+          value: db.id,
+        })
+      }
+      const sortedDbChoices = dbChoices.sort((a,b)=> {
+        return a.title.localeCompare(b.title)
+      })
+
+      const promptSelectedDbResult = await prompts([{
+        type: 'autocomplete',
+        name: 'database_id',
+        message: 'Select a database',
+        choices: sortedDbChoices
+      }], { onCancel })
+      console.log(promptSelectedDbResult)
+
+      databaseId = promptSelectedDbResult.database_id
+    }
+    const res = await notion.retrieveDb(databaseId)
     console.dir(res, { depth: null })
   }
 }
