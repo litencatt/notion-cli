d3f29613a486251ae341c9eef08daf55fabd2803
litencatt <ncl0709@gmail.com>
2023-08-13 12:07:46 +0900

    Add user test

--- test/commands/user/retrieve/bot.test.ts
+++ test/commands/user/retrieve/bot.test.ts
@@ -1,17 +1,39 @@
 import {expect, test} from '@oclif/test'
 
 describe('user:retrieve:bot', () => {
-  test
-  .stdout()
-  .command(['user:retrieve:bot'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
+  const response = {
+    object: 'user',
+    id: 'dummy-bot-id',
+    name: 'dummy-user-name',
+    avatar_url: null,
+    type: 'bot',
+    bot: {
+      owner: {
+        type: 'user',
+        user: {
+          object: 'user',
+          id: 'dummy-user-id',
+          name: 'dummy-user-name',
+          avatar_url: null,
+          type: 'person',
+          person: {
+            email: 'dummy-user-email',
+          },
+        },
+      }
+    },
+
+  }
 
   test
+  .nock('https://api.notion.com', api => api
+    .get('/v1/users/me')
+    .reply(200, response)
+  )
   .stdout()
-  .command(['user:retrieve:bot', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
+  .command(['user:retrieve:bot'])
+  .it('shows retrieved bot object when success', ctx => {
+    expect(ctx.stdout).to.contain("object: \'user")
+    expect(ctx.stdout).to.contain("type: \'bot")
   })
 })
