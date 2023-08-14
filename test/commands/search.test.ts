286ede7aee1fb7042119898f6673206348ec9e72
litencatt <ncl0709@gmail.com>
2023-08-14 17:04:45 +0900

    Add table format test

--- test/commands/search.test.ts
+++ test/commands/search.test.ts
@@ -7,10 +7,30 @@ describe('search', () => {
       {
         object: 'page',
         id: 'dummy-page-id',
+        url: 'https://www.notion.so/dummy-page-id',
+        properties: {
+          Name: {
+            id: 'title',
+            type: 'title',
+            title: [
+              {
+                type: 'text',
+                plain_text: 'dummy page title',
+              }
+            ]
+          }
+        }
       },
       {
         object: 'database',
         id: 'dummy-database-id',
+        url: 'https://www.notion.so/dummy-database-id',
+        title: [
+          {
+            type: 'text',
+            plain_text: 'dummy database title',
+          }
+        ]
       }
     ],
     next_cursor: null,
@@ -19,15 +39,32 @@ describe('search', () => {
     page_or_database: {}
   }
 
-  test
-  .nock('https://api.notion.com', api => api
+  const searchApiMock = test.nock('https://api.notion.com', api => api
     .post('/v1/search')
     .reply(200, response)
   )
-  .stdout()
-  .command(['search'])
-  .it('shows retrieved result object when success', ctx => {
-    expect(ctx.stdout).to.contain("object: \'list")
-    expect(ctx.stdout).to.contain("results: [")
+
+  describe('with no flags', () => {
+    searchApiMock
+    .stdout()
+    // Need --no-truncate flag to match expected stdout
+    .command(['search', '--no-truncate'])
+    .it('shows search result table', ctx => {
+      expect(ctx.stdout).to.match(/Title.*Object.*Id.*Url/)
+      expect(ctx.stdout).to.match(/dummy page title.*page.*dummy-page-id.*https:\/\/www\.notion\.so\/dummy-page-id/)
+      expect(ctx.stdout).to.match(/dummy database title.*database.*dummy-database-id.*https:\/\/www\.notion\.so\/dummy-database-id/)
+    })
+  })
+
+  describe('with --row flags', () => {
+    searchApiMock
+    .stdout()
+    .command(['search', '--row'])
+    .it('shows search result row json', ctx => {
+      expect(ctx.stdout).to.contain("object: \'list")
+      expect(ctx.stdout).to.contain("url: \'https://www.notion.so/dummy-page-id")
+      expect(ctx.stdout).to.contain("url: \'https://www.notion.so/dummy-database-id")
+      expect(ctx.stdout).to.contain("type: \'page_or_database")
+    })
   })
 })
