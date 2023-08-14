ef218a0ff768800c903da6595b5213195cdaec61
litencatt <ncl0709@gmail.com>
2023-08-14 19:50:56 +0900

    Support ux table to page:create

--- src/commands/page/create.ts
+++ src/commands/page/create.ts
@@ -1,12 +1,14 @@
-import {Args, Command, Flags} from '@oclif/core'
+import {Args, Command, Flags, ux} from '@oclif/core'
 import * as notion from '../../notion'
 import * as fs from 'fs'
 import * as path from 'path'
 import { markdownToBlocks } from '@tryfabric/martian'
 import {
   CreatePageParameters,
+  CreatePageResponse,
   BlockObjectRequest,
 } from '@notionhq/client/build/src/api-endpoints'
+import { isFullPage } from '@notionhq/client'
 
 export default class PageCreate extends Command {
   static description = 'Create a page'
@@ -28,47 +30,78 @@ export default class PageCreate extends Command {
       char: 'f',
       description: 'Path to a source markdown file',
     }),
+    row: Flags.boolean(),
+    ...ux.table.flags(),
   }
 
   public async run(): Promise<void> {
     const {args, flags} = await this.parse(PageCreate)
 
     let pageProps: CreatePageParameters
-    let parent: CreatePageParameters['parent']
-    if (!flags.filePath) {
-      if (flags.parent_page_id) {
-        parent = {
-          page_id: flags.parent_page_id,
-        }
-      } else {
-        parent = {
-          database_id: flags.parent_db_id,
-        }
-      }
-      pageProps = {
-        parent: parent,
-        properties: {}
+    let pageParent: CreatePageParameters['parent']
+    if (flags.parent_page_id) {
+      pageParent = {
+        page_id: flags.parent_page_id,
       }
     } else {
+      pageParent = {
+        database_id: flags.parent_db_id,
+      }
+    }
+
+    if (flags.filePath) {
       const p = path.join('./', flags.file_path)
-      const title = path.basename(flags.file_path)
+      const fileName = path.basename(flags.file_path)
       const md = fs.readFileSync(p, { encoding: 'utf-8' })
       const blocks = markdownToBlocks(md)
 
       // TODO: Add support for creating a page from a template
       pageProps = {
-        parent: {
-          database_id: args.parent_id,
-        },
+        parent: pageParent,
         properties: {
           Name: {
-            title: [{ text: {content: title} }],
+            title: [{ text: {content: fileName} }],
           },
         },
         children: blocks as BlockObjectRequest[],
       }
+    } else {
+      pageProps = {
+        parent: pageParent,
+        properties: {}
+      }
     }
     const res = await notion.createPage(pageProps)
-    console.dir(res, { depth: null })
+
+    if (flags.row) {
+      console.dir(res, { depth: null })
+      this.exit(0)
+    }
+
+    const columns = {
+      title: {
+        get: (row: CreatePageResponse) => {
+          if (isFullPage(row)) {
+            let title: string
+            Object.entries(row.properties).find(([_, prop]) => {
+              if (prop.type === 'title') {
+                title = prop.title[0] && prop.title[0].plain_text
+              }
+            })
+            return title
+          } else {
+            return 'untitled'
+          }
+        },
+      },
+      object: {},
+      id: {},
+      url: {},
+    }
+    const options = {
+      printLine: this.log.bind(this),
+      ...flags
+    }
+    ux.table([res], columns, options)
   }
 }
