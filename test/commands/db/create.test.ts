a6fa3578d835cd8874273c6f0524411bfeec9064
litencatt <ncl0709@gmail.com>
2023-08-18 23:26:38 +0900

    Test response database object title is []

--- test/commands/db/create.test.ts
+++ test/commands/db/create.test.ts
@@ -1,5 +1,12 @@
 import {expect, test} from '@oclif/test'
 
+const apiMock = (response: any) => {
+  return test.nock('https://api.notion.com', api => api
+    .post('/v1/databases')
+    .reply(200, response)
+  ).stdout({print: process.env.TEST_DEBUG ? true : false})
+}
+
 describe('db:create', () => {
   const response = {
     object: 'database',
@@ -16,13 +23,8 @@ describe('db:create', () => {
     url: 'https://www.notion.so/dummy-database-id',
   }
 
-  const apiMock = test.nock('https://api.notion.com', api => api
-    .post('/v1/databases')
-    .reply(200, response)
-  ).stdout({print: process.env.TEST_DEBUG ? true : false})
-
   describe('with no flags', () => {
-    apiMock
+    apiMock(response)
     .command([
       'db:create',
       '--no-truncate',
@@ -36,7 +38,7 @@ describe('db:create', () => {
   })
 
   describe('with --raw flags', () => {
-    apiMock
+    apiMock(response)
     .command([
       'db:create',
       'dummy-page-id',
@@ -47,6 +49,27 @@ describe('db:create', () => {
     .it('shows created database object when success with title flags', ctx => {
       expect(ctx.stdout).to.contain("dummy-database-id")
       expect(ctx.stdout).to.contain("dummy database title")
+    })
+  })
+
+  describe('response title is []', () => {
+    const titleEmptyResponse = {
+      object: 'database',
+      id: 'dummy-database-id',
+      title: [],
+      url: 'https://www.notion.so/dummy-database-id',
+    }
+
+    apiMock(titleEmptyResponse)
+    .command([
+      'db:create',
+      '--no-truncate',
+      '-t', 'dummy database title',
+      'dummy-page-id'
+    ])
+    .it('shows created result table', ctx => {
+      expect(ctx.stdout).to.match(/Title.*Object.*Id.*Url/)
+      expect(ctx.stdout).to.match(/Untitled.*database.*dummy-database-id.*https:\/\/www\.notion\.so\/dummy-database-id/)
     })
   })
 })
--- test/commands/db/update.test.ts
+++ test/commands/db/update.test.ts
@@ -1,28 +1,37 @@
 import {expect, test} from '@oclif/test'
 
-describe('db:update', () => {
-  const response = {
-    object: 'database',
-    id: 'dummy-database-id',
-    title: [
-      {
-        type: 'text',
-        text: {
-          content: 'dummy database title',
-        },
-        plain_text: 'dummy database title',
-      }
-    ],
-    url: 'https://www.notion.so/dummy-database-id',
-  }
-
-  const apiMock = test.nock('https://api.notion.com', api => api
+const apiMock = (response: any) => {
+  return test.nock('https://api.notion.com', api => api
     .patch('/v1/databases/dummy-database-id')
     .reply(200, response)
   ).stdout({print: process.env.TEST_DEBUG ? true : false})
+}
 
+const response = {
+  object: 'database',
+  id: 'dummy-database-id',
+  title: [
+    {
+      type: 'text',
+      text: {
+        content: 'dummy database title',
+      },
+      plain_text: 'dummy database title',
+    }
+  ],
+  url: 'https://www.notion.so/dummy-database-id',
+}
+
+const titleEmptyResponse = {
+  object: 'database',
+  id: 'dummy-database-id',
+  title: [],
+  url: 'https://www.notion.so/dummy-database-id',
+}
+
+describe('db:update', () => {
   describe('with no flags', () => {
-    apiMock
+    apiMock(response)
     .command([
       'db:update',
       '--no-truncate',
@@ -36,7 +45,7 @@ describe('db:update', () => {
   })
 
   describe('with --raw flags', () => {
-    apiMock
+    apiMock(response)
     .command([
       'db:update',
       '-t', 'dummy database title',
@@ -47,6 +56,20 @@ describe('db:update', () => {
     .it('shows updated database object', ctx => {
       expect(ctx.stdout).to.contain("dummy-database-id")
       expect(ctx.stdout).to.contain("dummy database title")
+    })
+  })
+
+  describe('response title is []', () => {
+    apiMock(titleEmptyResponse)
+    .command([
+      'db:update',
+      '--no-truncate',
+      '-t dummy database title',
+      'dummy-database-id'
+    ])
+    .it('shows updated result table', ctx => {
+      expect(ctx.stdout).to.match(/Title.*Object.*Id.*Url/)
+      expect(ctx.stdout).to.match(/Untitled.*database.*dummy-database-id.*https:\/\/www\.notion\.so\/dummy-database-id/)
     })
   })
 })
