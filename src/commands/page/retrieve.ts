2e753fe0642def51140a10ef21475d55f6d48629
litencatt <ncl0709@gmail.com>
2023-08-20 21:51:06 +0900

    Output markdown string

--- src/commands/page/retrieve.ts
+++ src/commands/page/retrieve.ts
@@ -8,6 +8,7 @@ import {
   getPageTitle,
   outputRawJson
 } from '../../helper'
+import { NotionToMarkdown } from "notion-to-md"
 
 export default class PageRetrieve extends Command {
   static description = 'Retrieve a page'
@@ -32,6 +33,10 @@ export default class PageRetrieve extends Command {
       char: 'r',
       description: 'output raw json',
     }),
+    markdown: Flags.boolean({
+      char: 'm',
+      description: 'output markdown',
+    }),
     ...ux.table.flags(),
   }
 
@@ -47,6 +52,13 @@ export default class PageRetrieve extends Command {
     const res = await notion.retrievePage(pageProps)
     if (flags.raw) {
       outputRawJson(res)
+      this.exit(0)
+    }
+    if (flags.markdown) {
+      const n2m = new NotionToMarkdown({ notionClient: notion.client })
+      const mdBlocks = await n2m.pageToMarkdown(args.page_id)
+      const mdString = n2m.toMarkdownString(mdBlocks)
+      console.log(mdString.parent)
       this.exit(0)
     }
 
