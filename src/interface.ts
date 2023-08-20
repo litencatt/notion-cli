36cd68c1f0be12136dddc4058c2c29837a59f25b
litencatt <ncl0709@gmail.com>
2023-08-20 22:02:35 +0900

    Remove not used interface

--- src/interface.ts
+++ src/interface.ts
@@ -1,33 +1,3 @@
-import {
-  GetDatabaseResponse,
-  PartialDatabaseObjectResponse
-} from '@notionhq/client/build/src/api-endpoints'
-
-export interface Setting {
-  name: string
-  enable: boolean
-  pDbId: string
-  cDbId: string
-  relationKeys: string
-  updateProp: string
-}
-
-export interface ParentPage {
-  page_id: string
-  relation_keys: RelationKey[]
-}
-
-export interface PropetyTypeInfo {
-  property: string
-  type: string
-  value?: string
-}
-
-export interface RelationKey {
-  property: string
-  value: string
-}
-
 export interface IPromptChoice {
   title: string
   value?: string
